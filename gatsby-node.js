bb01e0afec7c42dda4a32e5850745e45c3ca302a
ivanlhz <ivanlhz@gmail.com>
2020-04-04 09:38:07 +0100

    Creando las paginas y su template base

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -10,4 +11,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: createFilePath({ node, getNode, basePath: "pages" }),
     })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/blog-posts.js"),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
---
+++ src/templates/blog-posts.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPost = () => {
+  return (
+    <Layout>
+      <h1>Titulo del articulo</h1>
+      <h3>20/07/2019 - 4 min de lectura</h3>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        vitae ullam hic nostrum, quibusdam odit, tenetur, dignissimos deserunt
+        ea doloremque harum. Mollitia, voluptates! Dolor eaque odit sapiente
+        blanditiis dicta ea.
+      </div>
+    </Layout>
+  )
+}
+export default BlogPost
